a4903ba194132bfce826b71b15b431890fa7d21a
seemix <semikhalchuk@gmail.com>
2023-04-10 10:25:38 +0300

    cart start

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,7 @@ import './Cart.css';
 import { useDispatch, useSelector } from 'react-redux';
 import CloseIcon from '@mui/icons-material/Close';
 import { hideCart } from '../../store/appearance';
+import { Button } from '@mui/material';
 
 const Cart = () => {
     const { showCart } = useSelector(state => state.appearanceStore);
@@ -11,13 +12,16 @@ const Cart = () => {
     return (
         <div className={showCart ? 'cart_wrapper show_cart' : 'cart_wrapper'}>
             <div style={{ cursor: 'pointer' }} onClick={() => dispatch(hideCart())}>
-                <CloseIcon fontSize={'large'}/>
+                <CloseIcon fontSize={'large'} style={{position: 'absolute', top: '20px', left: '20px'}}/>
             </div>
             <h2>Cart</h2>
             <p>44444444444444</p>
             <p>44444444444444</p>
             <p>44444444444444</p>
             <p>44444444444444</p>
+            <Button variant={'contained'} sx={{width: '100%' }}>
+                Checkout
+            </Button>
         </div>
     );
 };
