9c3aedd2acfb09dbf84934b1f00b7e631d45a7a5
seemix <semikhalchuk@gmail.com>
2023-04-12 23:12:27 +0300

    auth continue

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -4,6 +4,9 @@ import './CartItem.css';
 
 const CartItem = () => {
     const [count, setCount] = useState(1);
+    const reduceCount = () => {
+        if (count > 1) setCount(count => count - 1);
+    }
     return (
         <div>
             <Card className={'cart_card'}>
@@ -11,13 +14,14 @@ const CartItem = () => {
                 <div className={'cart_item_wrapper'}>
                     <div>
                         <div><img
-                            src={'https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322685/hookah-smoking.jpg'}  alt={'img'}/>
+                            src={'https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322685/hookah-smoking.jpg'}
+                            alt={'img'}/>
                         </div>
                     </div>
                     <div style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
                         <div style={{ textAlign: 'center' }}><big><b>500Kr.</b></big></div>
                         <div>
-                            <Button onClick={() => setCount(count => count - 1)}><big>-</big></Button>
+                            <Button onClick={reduceCount}><big>-</big></Button>
                             <TextField size={'small'} style={{ width: '44px' }}
                                        className={'TextField-without-border-radius'} value={count}/>
                             <Button onClick={() => setCount(count => count + 1)}><big>+</big></Button>
